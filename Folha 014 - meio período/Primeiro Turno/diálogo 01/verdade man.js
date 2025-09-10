d718879f9bdb2d8e0ac4212c5512fe7c06c63e1f
Minorizin <minoricostareis362@gmail.com>
2025-09-10 20:00:50 -0300

    adições

---
+++ Folha 014 - meio período/Primeiro Turno/diálogo 01/verdade man.js
@@ -0,0 +1,22 @@
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
