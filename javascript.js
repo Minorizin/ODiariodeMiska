d19006b8804f0b66feb8edaf7076311a257a122d
Minorizin <minoricostareis362@gmail.com>
2026-03-16 14:46:43 -0300

    Folha 010 - Fechada

--- javascript.js
+++ javascript.js
@@ -56,9 +56,9 @@ const previewData = {
     },
     folha010: {
         title: "Folha 010 - culpa",
-        image: "img/folha010.jpg",
+        image: "img/fechado-preview.gif",
         description: "Existe algo que a razão não consegue abafar. (Continuação da Folha 009)",
-        status: "Disponível",
+        status: "Indisponível",
     },
     folha011: {
         title: "Folha 011 - Uma nova perturbação",
